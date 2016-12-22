1ae98a19ecf78dd7f333b96f6f910690f29dc42a
UltraFaceguy <kattreinkyle@gmail.com>
2016-12-21 23:09:32 -0500

    Adding new heal over time buff, for use with updated healing potions

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Healing.java
@@ -0,0 +1,62 @@
+ /*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015  Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2016 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.shatteredpixel.shatteredpixeldungeon.actors.buffs;
+
+import com.watabou.utils.Bundle;
+
+ public class Healing extends Buff {
+
+     protected int level;
+     private static final String LEVEL	= "level";
+
+     @Override
+     public void storeInBundle( Bundle bundle ) {
+         super.storeInBundle( bundle );
+         bundle.put( LEVEL, level );
+     }
+
+     @Override
+     public void restoreFromBundle( Bundle bundle ) {
+         super.restoreFromBundle( bundle );
+         level = bundle.getInt( LEVEL );
+     }
+
+	@Override
+	public boolean act() {
+		if (!target.isAlive()) {
+			return true;
+		}
+	    if (target.HP == target.HT) {
+            level -= target.HT / 20;
+        } else {
+            int restore = Math.min(1 + level / 2, 1 + target.HT / 10);
+
+            level -= restore;
+            target.heal(restore);
+        }
+
+        if (level <= 0) {
+            detach();
+        }
+		
+		return true;
+	}
+}
